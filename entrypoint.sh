36fe601d782c1e6af3212804a6120ae1dc000064
Manuel Alonso Carracedo <macarracedo@esei.uvigo.es>
2021-08-17 15:42:16 +0200

    Added local path variable to output file (bom.xml)

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,6 +22,7 @@ case $LANGUAGE in
             echo "[-] Error executing npm install. Stopping the action!"
             exit 1
         fi
+        path="bom.xml"
         BoMResult=$(cyclonedx-bom -s 1.1 -o bom.xml)
         ;;
     
@@ -32,6 +33,7 @@ case $LANGUAGE in
             echo "[-] Error executing pip freeze to get a requirements.txt with frozen parameters. Stopping the action!"
             exit 1
         fi
+        path="bom.xml"
         BoMResult=$(cyclonedx-py -o bom.xml)
         ;;
     
@@ -41,6 +43,7 @@ case $LANGUAGE in
             echo "[-] Error executing go build. Stopping the action!"
             exit 1
         fi
+        path="bom.xml"
         BoMResult=$(cyclonedx-go -o bom.xml)
         ;;
         
@@ -76,7 +79,7 @@ upload_bom=$(curl $INSECURE $VERBOSE -s --location --request POST $DTRACK_URL/ap
 --form "autoCreate=true" \
 --form "projectName=$GITHUB_REPOSITORY" \
 --form "projectVersion=$GITHUB_REF" \
---form "bom=@bom.xml")
+--form "bom=@$path")
 
 token=$(echo $upload_bom | jq ".token" | tr -d "\"")
 echo "[*] BoM file succesfully uploaded with token $token"
