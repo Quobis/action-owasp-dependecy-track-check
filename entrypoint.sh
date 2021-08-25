8167289de58b5e6056774dd007673130b4608d9e
Manuel Alonso Carracedo <macarracedo@esei.uvigo.es>
2021-08-25 15:03:30 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -119,7 +119,7 @@ echo "[*] BoM file succesfully generated"
 # Cyclonedx CLI conversion 
 echo "PATH : $PATH"
 cyclonedx-linux-x64 --version
-cyclonedx-linux-x64 convert --input-file $path --input-format autodetect --output-file sbom.xml --output-format xml_v1_2
+cyclonedx-linux-x64 convert --input-file bom.xml --output-file sbom.xml --output-format xml_v1_2
 
 # UPLOAD BoM to Dependency track server
 echo "[*] Uploading BoM file to Dependency Track server"
