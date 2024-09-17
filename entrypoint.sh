e89cc3446f6eaef3e0c442cb2fd5242e52265ea9
SitoRBJ <sitoruizbravo@hotmail.com>
2024-09-17 17:34:57 +0200

    Update logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -65,11 +65,8 @@ case $LANGUAGE in
 
     "java")
         echo "[*]  Processing Java BoM"
-        ls
-        pwd
         if [ ! $? = 0 ]; then
             echo "[-] Error executing Java build. Stopping the action!"
-            echo "$?"
             exit 1
         fi
         apt-get install --no-install-recommends -y build-essential default-jdk maven
