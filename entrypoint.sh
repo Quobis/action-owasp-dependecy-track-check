abfff8609094713de9de850c946cb1976e211467
cesarempathy <59477015+cesarempathy@users.noreply.github.co>
2024-10-15 10:38:29 +0200

    Add python

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,70 +15,101 @@ chmod +x /usr/bin/cyclonedx-cli
 
 cd $GITHUB_WORKSPACE
 
-# Loop through each path
-for path in $PATHS; do
-    echo "[*] Processing path: $path"
+java() {
+    # Loop through each path
+    for path in $PATHS; do
+        echo "[*] Processing path: $path"
 
-    cd "$path"
-    bom_file="$path/target/bom.xml"
+        cd "$path"
+        bom_file="$path/target/bom.xml"
 
-    echo "[*] BoM file successfully generated at $bom_file"
+        echo "[*] BoM file successfully generated at $bom_file"
 
-    # Cyclonedx CLI conversion
-    echo "[*] Cyclonedx CLI conversion for $bom_file"
-    cyclonedx-cli convert --input-file "$bom_file" --output-file sbom.json --output-format json --output-version v1_6
+        # Cyclonedx CLI conversion
+        echo "[*] Cyclonedx CLI conversion for $bom_file"
+        cyclonedx-cli convert --input-file "$bom_file" --output-file sbom.json --output-format json --output-version v1_6
 
-    # UPLOAD BoM to Dependency Track server
-    echo "[*] Uploading BoM file for $bom_file to Dependency Track server"
-    upload_bom=$(curl $INSECURE $VERBOSE -s --location --request POST $DTRACK_URL/api/v1/bom \
-    --header "X-Api-Key: $DTRACK_KEY" \
-    --header "Content-Type: multipart/form-data" \
-    --form "autoCreate=true" \
-    --form "projectName=$GITHUB_REPOSITORY-$path" \
-    --form "projectVersion=$GITHUB_REF" \
-    --form "bom=@sbom.json")
+        # UPLOAD BoM to Dependency Track server
+        echo "[*] Uploading BoM file for $bom_file to Dependency Track server"
+        upload_bom=$(curl $INSECURE $VERBOSE -s --location --request POST $DTRACK_URL/api/v1/bom \
+        --header "X-Api-Key: $DTRACK_KEY" \
+        --header "Content-Type: multipart/form-data" \
+        --form "autoCreate=true" \
+        --form "projectName=$GITHUB_REPOSITORY-$path" \
+        --form "projectVersion=$GITHUB_REF" \
+        --form "bom=@sbom.json")
 
-    token=$(echo $upload_bom | jq ".token" | tr -d "\"")
-    echo "[*] BoM file successfully uploaded with token $token for path $path"
+        token=$(echo $upload_bom | jq ".token" | tr -d "\"")
+        echo "[*] BoM file successfully uploaded with token $token for path $path"
 
-    if [ -z "$token" ]; then
-        echo "[-] The BoM file for $path has not been successfully processed by OWASP Dependency Track"
-        exit 1
-    fi
-
-    echo "[*] Checking BoM processing status for $path"
-    processing=$(curl $INSECURE $VERBOSE -s --location --request GET $DTRACK_URL/api/v1/bom/token/$token \
-    --header "X-Api-Key: $DTRACK_KEY" | jq '.processing')
-
-    counter=0
-    while [ "$processing" = true ]; do
-        sleep 5
-        processing=$(curl  $INSECURE $VERBOSE -s --location --request GET $DTRACK_URL/api/v1/bom/token/$token \
-        --header "X-Api-Key: $DTRACK_KEY" | jq '.processing')
-        if [ $((++counter)) -eq 10 ]; then
-            echo "[-] Timeout while waiting for processing result for path $path. Please check the OWASP Dependency Track status."
+        if [ -z "$token" ]; then
+            echo "[-] The BoM file for $path has not been successfully processed by OWASP Dependency Track"
             exit 1
         fi
+
+        echo "[*] Checking BoM processing status for $path"
+        processing=$(curl $INSECURE $VERBOSE -s --location --request GET $DTRACK_URL/api/v1/bom/token/$token \
+        --header "X-Api-Key: $DTRACK_KEY" | jq '.processing')
+
+        counter=0
+        while [ "$processing" = true ]; do
+            sleep 5
+            processing=$(curl  $INSECURE $VERBOSE -s --location --request GET $DTRACK_URL/api/v1/bom/token/$token \
+            --header "X-Api-Key: $DTRACK_KEY" | jq '.processing')
+            if [ $((++counter)) -eq 10 ]; then
+                echo "[-] Timeout while waiting for processing result for path $path. Please check the OWASP Dependency Track status."
+                exit 1
+            fi
+        done
+
+        echo "[*] OWASP Dependency Track processing completed for $path"
+
+        # Wait to make sure the score is available
+        sleep 5
+
+        echo "[*] Retrieving project information for $path"
+        project=$(curl  $INSECURE $VERBOSE -s --location --request GET "$DTRACK_URL/api/v1/project/lookup?name=$GITHUB_REPOSITORY-$path&version=$GITHUB_REF" \
+        --header "X-Api-Key: $DTRACK_KEY")
+
+        echo "[*] Project: $project"
+
+        project_uuid=$(echo $project | jq ".uuid" | tr -d "\"")
+        risk_score=$(echo $project | jq ".lastInheritedRiskScore")
+        echo "[*] Project risk score for $path: $risk_score"
+
+        # Append this path and its risk score to the output string
+        output="${output}Path: $path, Risk Score: $risk_score\n"
     done
+}
 
-    echo "[*] OWASP Dependency Track processing completed for $path"
+python() {
+    echo "[*]  Processing Python BoM"
+    apt-get install --no-install-recommends -y python3 python3-pip
+    freeze=$(pip freeze > requirements.txt)
+    if [ ! $? = 0 ]; then
+        echo "[-] Error executing pip freeze to get a requirements.txt with frozen parameters. Stopping the action!"
+        exit 1
+    fi
+    pip install cyclonedx-bom
+    path="bom.xml"
+    BoMResult=$(cyclonedx-py requirements -o bom.xml)
+}
 
-    # Wait to make sure the score is available
-    sleep 5
+java
 
-    echo "[*] Retrieving project information for $path"
-    project=$(curl  $INSECURE $VERBOSE -s --location --request GET "$DTRACK_URL/api/v1/project/lookup?name=$GITHUB_REPOSITORY-$path&version=$GITHUB_REF" \
-    --header "X-Api-Key: $DTRACK_KEY")
+case $LANGUAGE in
+    "java")
+        java
+        ;;
 
-    echo "[*] Project: $project"
-
-    project_uuid=$(echo $project | jq ".uuid" | tr -d "\"")
-    risk_score=$(echo $project | jq ".lastInheritedRiskScore")
-    echo "[*] Project risk score for $path: $risk_score"
-
-    # Append this path and its risk score to the output string
-    output="${output}Path: $path, Risk Score: $risk_score\n"
-done
+    "python")
+        python
+        ;;
+    *)
+        echo "[-] Unsupported language: $LANGUAGE"
+        exit 1
+        ;;
+esac
 
 # Output the final result with all paths and their risk scores
 echo -e "::set-output name=riskscores::$output"
